0eb6111c7642ba2505a3c517308de743759d2582
Mathprogrammer5 <j.flaig@progymnasium-alpirsbach.de>
2024-03-05 20:25:43 +0100

    Build script modified

--- bin/build.sh
+++ bin/build.sh
@@ -4,3 +4,4 @@ mojo package ./src/DSA -o ./bin/DSA.📦
 cp ./bin/DSA.📦 ./src/tests/DSA.📦
 mojo build ./src/tests/random_vector_test.mojo -o ./bin/tests/random_vector_test
 mojo build ./src/tests/vector_iter_test.mojo -o ./bin/tests/vector_iter_test
+cp ./bin/DSA.📦 ../Nexus/bin/DSA.📦
