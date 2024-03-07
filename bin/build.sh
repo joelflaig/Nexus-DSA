ddc00b15fd51adc0ef499c21f0a4b628c2435099
Mathprogrammer5 <j.flaig@progymnasium-alpirsbach.de>
2024-03-07 21:54:50 +0100

    Build script updated

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,7 @@
 # mojo package ./src/DSA/datastructs -o ./bin/datastructs.📦
 # cp ./bin/datastructs.📦 ./src/DSA/algorithms/datastructs.📦
-mojo package ./src/DSA -o ./bin/DSA.📦
-cp ./bin/DSA.📦 ./src/tests/DSA.📦
+mojo package ./src/DSA -o ./bin/DSA.mojopkg
+cp ./bin/DSA.mojopkg ./src/tests/DSA.mojopkg
 mojo build ./src/tests/random_vector_test.mojo -o ./bin/tests/random_vector_test
 mojo build ./src/tests/vector_iter_test.mojo -o ./bin/tests/vector_iter_test
-cp ./bin/DSA.📦 ../Nexus/bin/DSA.📦
+cp ./bin/DSA.mojopkg ../Nexus/bin/DSA.mojopkg
